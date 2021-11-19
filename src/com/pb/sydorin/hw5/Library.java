e147559dd495d82c943b9545ea7a47a2f76fa984
darksmoke <dsmoke@ukr.net>
2021-11-19 22:41:00 +0200

    Домашка 5

--- src/com/pb/sydorin/hw5/Library.java
+++ src/com/pb/sydorin/hw5/Library.java
@@ -14,21 +14,30 @@ public class Library {
         Book book3 = new Book("Шапка и орел","Рахманова О. Ю.", 2010);
 
         Reader reader1 = new Reader(101, "Смирнов А. А.",  "Экономический", "18/09/1999", "+380671231122");
+        Reader reader2 = new Reader(102, "Алексеенко П. Н.",  "Биологический", "03/11/2001", "+380501231155");
 
         System.out.println(book1.getBookInfo());
         System.out.println(book2.getBookInfo());
         System.out.println(book3.getBookInfo());
 
         System.out.println(reader1.getReadersInfo());
+        System.out.println(reader2.getReadersInfo());
 
         reader1.takeBook(3);
         reader1.returnBook(2);
         reader1.takeBook(1);
 
+        reader2.takeBook(5);
+        reader2.returnBook(2);
+        reader2.takeBook(2);
+
         reader1.takeBook(book1.getBookTitle(), book2.getBookTitle(), book3.getBookTitle());
+        reader1.takeBook(book1.getBookTitle(), book2.getBookTitle());
+        reader1.takeBook(book1.getBookInfo(), book2.getBookInfo());
 
-
-
+        reader2.returnBook(book1.getBookTitle(), book2.getBookTitle(), book3.getBookTitle());
+        reader2.returnBook(book1.getBookTitle(), book2.getBookTitle());
+        reader2.returnBook(book1.getBookInfo(), book2.getBookInfo());
 
     }
 }
--- src/com/pb/sydorin/hw5/Reader.java
+++ src/com/pb/sydorin/hw5/Reader.java
@@ -84,7 +84,7 @@ public class Reader {
 
     public void takeBook(int countBook) {
         this.countBook += countBook;
-        System.out.println(fio + " взял " + this.countBook + " книг.");
+        System.out.println(fio + " взял " + this.countBook + " книгу(и).");
     }
 
     public void takeBook(String... bookTitle) {
@@ -94,9 +94,25 @@ public class Reader {
         }
     }
 
+    public void takeBook(Book... book) {
+        System.out.println(book[0]);
+    }
+
+
     public void returnBook(int countBook) {
         this.countBook -= countBook;
         System.out.println(fio + " вернул " + this.countBook + " книгу(и).");
+    }
+
+    public void returnBook(String... bookTitle) {
+        System.out.println(this.fio + " вернул книги: ");
+        for (String book : bookTitle) {
+            System.out.println("- " + book);
+        }
+    }
+
+    public void returnBook(Book... book) {
+        System.out.println(book[0]);
     }
 
 }
